1b722506e3d88baf2efd1eebca5f4672baf4c16d
Laura Ilves <laura.susanne.ilves@gmail.com>
2021-12-05 13:40:45 -0500

    iconview switch init

--- src/components/search-bar/SearchBar.js
+++ src/components/search-bar/SearchBar.js
@@ -1,24 +1,34 @@
 import React, {useState} from 'react'
 import ViewToolbar from '../view-toolbar/ViewToolbar'
+import Button from '../card-view/Button';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faGripHorizontal } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faBars);
+library.add(faBars, faGripHorizontal);
 
 const SearchBar = (props) => {
+    const [listViewOn, setListViewOn] = useState(false)
     const search = (event) => {
         props.searchValue(event.target.value)
+    }
+    const toggleListView = (event) => {
+        setListViewOn(!listViewOn);
     }
 
     return (
         <div className="search-bar align-right">
             <input type="text" placeholder="Search..." onChange={search}></input>
-            <button className="bttn clear-bttn" onClick={search}>Clear</button>
-            <ViewToolbar>
-          <div className="icon-button">          <FontAwesomeIcon icon="bars" /><span className="icon-label">List View</span></div>
-        </ViewToolbar> 
+            <button className="bttn toolbar-bttn" onClick={search}>Clear</button>
+            <ViewToolbar onClick="setListViewOn">
+
+                {listViewOn ?                 <button className="bttn toolbar-bttn" onClick={toggleListView}><FontAwesomeIcon icon="grip-horizontal" /><span className="icon-label">Icon View</span></button> : <button className="bttn toolbar-bttn" onClick={toggleListView}>      
+                    <FontAwesomeIcon icon="bars" /><span className="icon-label">List View</span>
+                </button>}
+
+
+            </ViewToolbar> 
         </div>
     )
 }
